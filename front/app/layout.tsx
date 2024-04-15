e4d8b5937c26418456b25c3b727dc2fa9ac3ccb9
onsato <nogiglee@gmail.com>
2024-04-16 01:20:44 +0900

    linter, formatterの設定

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,36 +1,40 @@
-import Footer from "@/public/components/footer";
-import Header from "@/public/components/header";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Head from "next/head";
+import Footer from '@/public/components/footer';
+import Header from '@/public/components/header';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
-  title: "onc-limb",
-  description: "onc-limb home page",
+    title: 'onc-limb',
+    description: 'onc-limb home page',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <Head>
-        <title>ONC-LIMB</title>
-        <meta name="onc-limb" content="onc-limbのホームページ" />
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
-      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
-      <Header/>
-        {children}
-      <Footer/>
-
-        </body>
-    </html>
-  );
+    return (
+        <html lang="ja">
+            <Head>
+                <title>ONC-LIMB</title>
+                <meta name="onc-limb" content="onc-limbのホームページ" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <body
+                className={cn(
+                    'min-h-screen bg-background font-sans antialiased',
+                    inter.variable,
+                )}
+            >
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,16 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 const Home = () => {
-  return (
-      <main>
-        <h1>onc-limb（おんくらいむ）と申します</h1>
-        <h2>開発は楽しみだ</h2>
-        <div>
-          <Button>Click me</Button>
-        </div>
-        <h2>毎日に少しずつの成長を</h2>
-        <h2>こんな人間です！</h2>
-      </main>
-  );
-}
+    return (
+        <main>
+            <h1>onc-limb（おんくらいむ）と申します</h1>
+            <h2>開発は楽しみだ</h2>
+            <div>
+                <Button>Click me</Button>
+            </div>
+            <h2>毎日に少しずつの成長を</h2>
+            <h2>こんな人間です！</h2>
+        </main>
+    );
+};
 export default Home;
