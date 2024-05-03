10f1a27e11d647594c615815185929be745acd3e
onsato <nogiglee@gmail.com>
2024-05-03 12:49:27 +0900

    back環境構築

---
+++ back/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"back/graph"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+const defaultPort = "1323"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", welcome())
+
+	graphqlHandler := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	playgroundHandler := playground.Handler("GraphQL", "/api")
+
+	e.GET("/playground", func(c echo.Context) error {
+		playgroundHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+	e.POST("/api", func(c echo.Context) error {
+		graphqlHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	log.Printf("connect to http://localhost:%s/playground for GraphQL playground", port)
+	log.Fatalln(e.Start(":" + port))
+}
+
+func welcome() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome!")
+	}
+}
