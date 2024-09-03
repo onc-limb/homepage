be19ca87a7d8603931a5e8edfffe19d1bbf4e92d
onsato <nogiglee@gmail.com>
2024-09-03 23:42:15 +0900

    svelteの型解決

--- desktop/frontend/src/App.svelte
+++ desktop/frontend/src/App.svelte
@@ -1,79 +1,28 @@
 <script lang="ts">
-  import logo from './assets/images/logo-universal.png'
-  import {Greet} from '../wailsjs/go/main/App.js'
+  import { Router, Route, Link } from "svelte-routing";
+  import View from "./View.svelte";
+  import Insert from "./Insert.svelte";
+  import {
+    RouterType,
+    RouteType,
+    LinkType,
+    ViewType,
+    InsertType,
+  } from "./svelteComponent";
 
-  let resultText: string = "Please enter your name below 👇"
-  let name: string
-
-  function greet(): void {
-    Greet(name).then(result => resultText = result)
-  }
+  const RouterComponent = Router as typeof RouterType;
+  const RouteComponent = Route as typeof RouteType;
+  const LinkComponent = Link as typeof LinkType;
+  const ViewComponent = View as typeof ViewType;
+  const InsertComponent = Insert as typeof InsertType;
 </script>
 
-<main>
-  <img alt="Wails logo" id="logo" src="{logo}">
-  <div class="result" id="result">{resultText}</div>
-  <div class="input-box" id="input">
-    <input autocomplete="off" bind:value={name} class="input" id="name" type="text"/>
-    <button class="btn" on:click={greet}>Greet</button>
-  </div>
-</main>
+<RouterComponent>
+  <nav>
+    <LinkComponent to="/">View</LinkComponent>
+    <LinkComponent to="/insert">Insert</LinkComponent>
+  </nav>
 
-<style>
-
-  #logo {
-    display: block;
-    width: 50%;
-    height: 50%;
-    margin: auto;
-    padding: 10% 0 0;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-origin: content-box;
-  }
-
-  .result {
-    height: 20px;
-    line-height: 20px;
-    margin: 1.5rem auto;
-  }
-
-  .input-box .btn {
-    width: 60px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 3px;
-    border: none;
-    margin: 0 0 0 20px;
-    padding: 0 8px;
-    cursor: pointer;
-  }
-
-  .input-box .btn:hover {
-    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
-    color: #333333;
-  }
-
-  .input-box .input {
-    border: none;
-    border-radius: 3px;
-    outline: none;
-    height: 30px;
-    line-height: 30px;
-    padding: 0 10px;
-    background-color: rgba(240, 240, 240, 1);
-    -webkit-font-smoothing: antialiased;
-  }
-
-  .input-box .input:hover {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-  .input-box .input:focus {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-</style>
+  <RouteComponent path="/" component={ViewComponent} />
+  <RouteComponent path="/insert" component={InsertComponent} />
+</RouterComponent>
