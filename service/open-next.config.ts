f43df6d65b0392df744df58b6d0d32238840450a
onsato <nogiglee@gmail.com>
2025-10-04 11:50:07 +0900

    wrangler

--- service/open-next.config.ts
+++ service/open-next.config.ts
@@ -1,7 +1,3 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-
-export default defineCloudflareConfig({
-	incrementalCache: r2IncrementalCache,
-});
+import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
+export default defineCloudflareConfig({});
