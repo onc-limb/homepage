cd84477a15be44eb904bfb655091ec6c9636fd7d
onc-limb <onclimb.eng@gmail.com>
2026-02-07 15:52:43 +0900

    isr

--- service/app/books/page.tsx
+++ service/app/books/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react"
 import BooksContent from "./BooksContent"
 import { getBooks, getBookTags } from "@/lib/books"
 
+export const revalidate = 60
+
 export default async function BooksPage() {
     const [initialBooks, allTags] = await Promise.all([
         getBooks({ isRead: true, sort: "title", order: "asc" }),
